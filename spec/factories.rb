bfaa8085dceec6ec1e2815484b6c82ed6098bea9
saasbook <saasbook@saasbook.(none)>
2013-03-20 20:38:59 +0100

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
 	factory :user do
-		name "Michael Oczkowski"
-		email "michal@e-oczkowski.pl"
-		password "kaczor"
-		password_confirmation "kaczor"
+		sequence( :name ){ |n| "Person #{n}" }
+		sequence( :email ){ |n| "person_#{n}@example.com" }
+		password "foobar"
+		password_confirmation "foobar"
+
+		factory :admin do
+			admin true
+		end
 	end
 end
