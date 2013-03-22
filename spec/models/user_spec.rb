c8666b756f465bdd254a89a327490722bd738e59
saasbook <saasbook@saasbook.(none)>
2013-03-22 19:04:26 +0100

    Add user microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,11 +23,21 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
 
   it { should respond_to(:admin) }
 
   it { should be_valid }
   it { should_not be_admin }
+
+  describe "can't set admin on user" do
+	  it "should raise error" do
+	  	expect do 
+	  		@user.attributes = { :admin => true }
+	  	end.to raise_error( ActiveModel::MassAssignmentSecurity::Error )
+	  end
+	end
 
   describe "with admin attribute set to 'true'" do
   	before do
@@ -123,5 +133,39 @@ describe User do
 	describe "remember token" do 
 		before { @user.save }
 		its( :remember_token ){ should_not be_blank }
+	end
+
+	describe "microposts associations" do
+
+		before { @user.save }
+		let!(:older_micropost) do
+			FactoryGirl.create( :micropost, user: @user, created_at: 1.day.ago )
+		end
+		let!(:newer_micropost) do
+			FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago )
+		end
+
+		it "should destroy associated microposts" do
+			microposts = @user.microposts.dup
+			@user.destroy
+			microposts.should_not be_empty
+			microposts.each do |micropost|
+				Micropost.find_by_id( micropost.id ).should be nil
+			end
+		end
+
+		it "should have the right microposts in the right order" do
+			@user.microposts.should == [newer_micropost, older_micropost]
+		end
+
+		describe "status" do
+			let(:unfallowed_post) do
+				FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+			end
+
+			its(:feed) { should include(newer_micropost) }
+			its(:feed) { should include(older_micropost) }
+			its(:feed) { should_not include( unfallowed_post ) }
+		end
 	end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -15,6 +15,8 @@ describe "AuthenticationPages" do
 
   		it { should have_selector( 'title', text: 'Sign in' ) }
   		it { should have_selector( 'div.alert.alert-error', text: 'Invalid' ) }
+      it { should_not have_link('Profile' ) }
+      it { should_not have_link( 'Settings') }
 
   		describe "after visiting another page" do
   			before { click_link "Home" }
@@ -90,6 +92,20 @@ describe "AuthenticationPages" do
           before { visit users_path }
           it { should have_selector( 'title', text: 'Sign in' ) }
         end
+
+        describe "in the Microposts controller" do
+
+          describe "submitting to the create action" do
+            before { post microposts_path }
+            specify { response.should redirect_to(signin_path) }
+          end
+
+          describe "submitting to the destroy action" do
+            before { delete micropost_path( FactoryGirl.create( :micropost ) ) }
+            specify { response.should redirect_to( signin_path ) }
+          end
+        end
+
       end
     end
 
